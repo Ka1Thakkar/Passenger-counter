9e1eae60755223eff484d41283f71670d84d796a
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-03-30 18:08:07 +0530

    Edits

--- index.js
+++ index.js
@@ -4,12 +4,39 @@
 
 // camelCase
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let totalEl = document.getElementById("total-el")
 
 console.log(countEl)
 
 let count = 0
+let total = 0
+
+totalEl.textContent = total
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
 }
+
+function save() {
+    let countStr = " - " + count
+    if (saveEl.textContent == "Previous entries: ") {
+        saveEl.textContent += count
+    }
+    else {
+        saveEl.textContent += countStr
+    }
+    total += count
+    totalEl.textContent = total
+    count = 0
+    countEl.textContent = 0
+}
+
+function reset(){
+    count = 0
+    total = 0
+    saveEl.textContent = "Previous entries: "
+    totalEl.textContent = total
+}
+
